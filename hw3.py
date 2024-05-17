a5944f9b1f40ae084aade07efe5cc6938f1b5249
yuc0437 <b405110031@tmu.edu.tw>
2024-05-18 01:12:37 +0800

    add hw3.py

---
+++ hw3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def QuickSort(array, start, end):
+    if start >= end:
+        return
+    
+    pivot_index = start
+    pivot = array[pivot_index]
+    left = start + 1
+    right = end
+    
+    while left <= right:
+        while left <= right and array[left] <= pivot:
+            left += 1
+        while left <= right and array[right] >= pivot:
+            right -= 1
+        if left < right:
+            array[left], array[right] = array[right], array[left]
+            print(array)
+    
+    array[start], array[right] = array[right], array[start]
+    print(array)
+    
+    QuickSort(array, start, right - 1)
+    
+    QuickSort(array, right + 1, end)
+
+array = [33, 67, 8, 13, 54, 119, 3, 84, 25, 41]
+print(f"Initial array: {array}")
+
+QuickSort(array, 0, len(array) - 1)
+
+print(f"Sorted array: {array}")
+
+
+# In[3]:
+
+
+get_ipython().system('jupyter nbconvert --to script first_code.ipynb')
+
+
+# In[ ]:
+
+
+
+
